e2b273580bc1fea5592c1cfedb1aabc163e8a5d6
Joseph Pickrell <joepickrell@Josephs-MacBook-Pro.local>
2017-09-19 18:21:56 -0400

    make sure it waits?

--- lift_over.py
+++ lift_over.py
@@ -28,7 +28,8 @@ def lift_over(chrom, pos, strand, chain):
             # $ ./liftOver <oldFile> <map.chain> <newFile> <unMapped>
             cmd = [lift_over_exec, query.name, map_chain, mapped.name, unmapped.name]
             # print >>sys.stderr, '[DEBUG]', cmd
-            subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
+            p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+	    p.wait()
 
             mapped.seek(0)
             for line in mapped:
